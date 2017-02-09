3469eec67fa51a4d8cc0781dd1a0d36395c8f393
roman@kislovs.ru <roman@kislovs.ru>
2017-02-09 19:16:36 +0500

    UserController with CRUD

---
+++ DocDesckApp/Controllers/UserController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Web.Mvc;
+using DocDesckApp.Models;
+using System.Data.Entity;
+
+namespace DocDesckApp.Controllers
+{
+    [Authorize(Roles ="Администратор, Модератор, Исполнитель")]
+    public class UserController : Controller
+    {
+        private DocDeskContext db = new DocDeskContext();
+        [HttpGet]
+        // GET: User
+        public ActionResult Index()
+        {
+            var users = db.Users.Include(u => u.Department).Include(u => u.Role).ToList();
+            return View(users);
+        }
+        [HttpGet]
+        [Authorize(Roles ="Администратор")]
+        public ActionResult Create()
+        {
+            SelectList departments = new SelectList(db.Departments, "Id", "Name");
+            ViewBag.Departments = departments;
+            SelectList roles = new SelectList(db.Roles, "Id", "Name");
+            ViewBag.Roles = roles;
+            return View();
+        }
+        [HttpPost]
+        [Authorize(Roles ="Администратор")]
+        public ActionResult Create(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Users.Add(user);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            SelectList departments = new SelectList(db.Departments, "Id", "Name");
+            ViewBag.Departments = departments;
+            SelectList roles = new SelectList(db.Roles, "Id", "Name");
+            ViewBag.Roles = roles;
+            
+
+            return View(user);
+        }
+        [HttpGet]
+        [Authorize(Roles ="Администратор")]
+        public ActionResult Edit(Guid id)
+        {
+            User user = db.Users.Find(id);
+            SelectList departments = new SelectList(db.Departments, "Id", "Name", user.DepartmentId);
+            ViewBag.Departments = departments;
+            SelectList roles = new SelectList(db.Roles, "Id", "Name", user.RoleId);
+            ViewBag.Roles = roles;
+
+            return View(user);
+        }
+        [HttpPost]
+        [Authorize(Roles = "Администратор")]
+        public ActionResult Edit(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(user).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            SelectList departments = new SelectList(db.Departments, "Id", "Name");
+            ViewBag.Departments = departments;
+            SelectList roles = new SelectList(db.Roles, "Id", "Name");
+            ViewBag.Roles = roles;
+
+
+            return View(user);
+
+        }
+        [Authorize(Roles = "Администратор")]
+        public ActionResult Delete(Guid id)
+        {
+            User user = db.Users.Find(id);
+            db.Users.Remove(user);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
