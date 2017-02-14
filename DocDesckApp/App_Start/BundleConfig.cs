df2f766db75e4bf22c1b4abae8bce3f79513c3aa
roman@kislovs.ru <roman@kislovs.ru>
2017-02-14 08:26:30 +0500

    User filter

---
+++ DocDesckApp/App_Start/BundleConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace DocDesckApp.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundle(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                "~/Scripts/jqure-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+                "~/Scripts/jqure-ui-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                "~/Scripts/jqure.unobstrusive*",
+                 "~/Scripts/jqure.validate*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                "~/Scripts/modernizr-*"));
+        }
+    }
+}
--- DocDesckApp/Controllers/UserController.cs
+++ DocDesckApp/Controllers/UserController.cs
@@ -9,7 +9,7 @@ using System.Data.Entity;
 
 namespace DocDesckApp.Controllers
 {
-    [Authorize(Roles = "Администратор")]
+    [Authorize(Roles = "Администратор, Модератор, Исполнитель")]
     public class UserController : Controller
     {
         private DocDeskContext db = new DocDeskContext();
@@ -17,8 +17,57 @@ namespace DocDesckApp.Controllers
         // GET: User
         public ActionResult Index()
         {
+            
             var users = db.Users.Include(u => u.Department).Include(u => u.Role).ToList();
+            //добавляем выбор всех
+            List<Department> departments = db.Departments.ToList();
+            departments.Insert(0, new Department { Name = "Все", Id= Guid.Empty });
+            ViewBag.Departments = new SelectList(departments, "Id", "Name");
+            List<Role> roles = db.Roles.ToList();
+            roles.Insert(0, new Role { Name = "Все", Id = Guid.Empty });
+            ViewBag.Roles = new SelectList(roles, "Id", "Name");
+
             return View(users);
+        }
+        //поиск пользователей по департаменту и статусу
+        [HttpPost]
+        public ActionResult Index(Guid department, Guid role)
+        {
+            IEnumerable<User> allusers = null;
+            if (role == Guid.Empty && department == Guid.Empty)
+            {
+                return RedirectToAction("Index");
+
+            }
+
+            if (role == Guid.Empty && department != Guid.Empty)
+            {
+                allusers = from user in db.Users.Include(u => u.Department).Include(u => u.Role)
+                           where user.DepartmentId == department
+                           select user;
+             }
+            else if (role != Guid.Empty && department == Guid.Empty)
+            {
+                allusers = from user in db.Users.Include(u => u.Department).Include(u => u.Role)
+                           where user.RoleId == role
+                           select user;
+            }
+            else
+            {
+                allusers = from user in db.Users.Include(u => u.Department).Include(u => u.Role)
+                           where user.DepartmentId == department && user.RoleId == role
+                           select user;
+            }
+            //добавляем выбор всех
+            List<Department> departments = db.Departments.ToList();
+            departments.Insert(0, new Department { Name = "Все", Id = Guid.Empty });
+            ViewBag.Departments = new SelectList(departments, "Id", "Name");
+            List<Role> roles = db.Roles.ToList();
+            roles.Insert(0, new Role { Name = "Все", Id = Guid.Empty });
+            ViewBag.Roles = new SelectList(roles, "Id", "Name");
+
+
+            return View(allusers.ToList());
         }
         [HttpGet]
         [Authorize(Roles ="Администратор")]
@@ -36,6 +85,7 @@ namespace DocDesckApp.Controllers
         {
             if (ModelState.IsValid)
             {
+                user.Id = Guid.NewGuid();
                 db.Users.Add(user);
                 db.SaveChanges();
                 return RedirectToAction("Index");
