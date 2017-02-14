dcbda09e1d2b798edbf9b41ada0ecff440085ce2
roman@kislovs.ru <roman@kislovs.ru>
2017-02-14 09:15:36 +0500

    Department editor

---
+++ DocDesckApp/Controllers/ServiceController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using DocDesckApp.Models;
+
+namespace DocDesckApp.Controllers
+{
+    [Authorize (Roles = "Администратор")]
+    public class ServiceController : Controller
+    {
+        DocDeskContext db = new DocDeskContext();
+        // GET: Service
+       [HttpGet]
+       public ActionResult Departments()
+        {
+            ViewBag.Departments = db.Departments;
+            return View();
+        }
+        //добавление отдела с последующим отображением
+        [HttpPost]
+        public ActionResult Departments(Department depo)
+        {
+            if (ModelState.IsValid)
+            {
+                depo.Id = Guid.NewGuid();
+                db.Departments.Add(depo);
+                db.SaveChanges();
+            }
+            ViewBag.Departments = db.Departments;
+            return View(depo);
+        }
+
+        //удаление отдела по id
+        public ActionResult DeleteDepartment(Guid id)
+        {
+            Department depo = db.Departments.Find(id);
+            db.Departments.Remove(depo);
+            db.SaveChanges();
+            return RedirectToAction("Departments");
+        }
+    }
+}
